118808ac2bf0ef721a78e943808b851beb413ba9
Brian Kozub <ticet11@gmail.com>
2026-02-20 01:59:21 -0500

    3 themes, toggleable with left and right theme changer buttons.

--- main.js
+++ main.js
@@ -11,8 +11,12 @@ const footer_images = document.getElementsByClassName("footer-img");
 const body = document.getElementsByTagName("body");
 const container = document.getElementsByClassName("container");
 const header = document.getElementsByClassName("header");
-const themeChanger = () => {
-	currentThemeIndex = (currentThemeIndex + 1) % themes.length;
+const themeChanger = (direction) => {
+	if (direction === "left") {
+		currentThemeIndex = (currentThemeIndex - 1) % themes.length;
+	} else if (direction === "right") {
+		currentThemeIndex = (currentThemeIndex + 1) % themes.length;
+	}
 	console.log(currentThemeIndex);
 	console.log(header);
 
@@ -37,17 +41,17 @@ const themeChanger = () => {
 		body[0].style.fontFamily = fonts[theme_key];
 		container[0].style.background = "#140101";
 		container[0].style.color = "#f4f4f4";
-        header[0].style.color = "#e9e9e9";
+		header[0].style.color = "#e9e9e9";
 		for (let i = 0; i < header_images.length; i++) {
 			header_images[i].style.visibility = "visible";
 			header_images[i].src = `./assets/themes/${theme_key}/header-img.gif`;
 		}
 		for (let i = 0; i < section_header_images.length; i++) {
-            section_header_images[i].style.visibility = "visible";
+			section_header_images[i].style.visibility = "visible";
 			section_header_images[i].src = `./assets/themes/${theme_key}/section-header-img.gif`;
 		}
 		for (let i = 0; i < footer_images.length; i++) {
-            footer_images[i].style.visibility = "visible";
+			footer_images[i].style.visibility = "visible";
 			footer_images[i].src = `./assets/themes/${theme_key}/footer-img.gif`;
 		}
 	}
